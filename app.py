4e6cb99a411dff093ddc35411026d44cc26a4aa4
ss-518 <sanjeev25sanju@gmail.com>
2026-04-09 19:48:32 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ else:
 
 if api_key:
     genai.configure(api_key=api_key)
-    model = genai.GenerativeModel("gemini-1.5-flash")
+    model = genai.GenerativeModel("gemini-2.5-flash")
 
     # 3. --- CHAT HISTORY LOGIC ---
     # Initialize chat history if it doesn't exist
